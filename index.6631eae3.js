6129fee7e07e695b7c665f1768676f3d6e28d6cf
OlehScherbak <103534824+OlehScherbak@users.noreply.github.com>
2022-11-28 14:01:33 +0000

    Deploying to gh-pages from @ OlehScherbak/goit-js-hw-10@fd4e1fc06d889f77c945cebdf3a1ae90b8ccbb44 🚀

--- index.6631eae3.js
+++
@@ -1,2 +0,0 @@
-console.log(fetch(`https://restcountries.com/v3/name/${"ukr"}?fields=name.official,capital,population,flags,languages`).then((o=>o.json())).then((o=>{console.log(o)})).catch((o=>{console.log(o)})));
-//# sourceMappingURL=index.6631eae3.js.map
